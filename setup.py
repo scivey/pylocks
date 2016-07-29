a78f84208f5405a4c24ccf94b3f6ea63f1cbc72a
Scott Ivey <scott.ivey@gmail.com>
2016-07-28 20:17:38 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = [
+    'redis>=2.10.5'
+]
+
+setup(
+    name='pylocks',
+    version='0.1.0',
+    description="redis-backed locks and leases",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=INSTALL_REQUIRES,
+    author='Scott Ivey',
+    author_email='scott.ivey@gmail.com',
+    license='MIT',
+    packages=find_packages()
+)
